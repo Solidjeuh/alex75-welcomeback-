fb8d03b6e4786e1b4261312e323fff5c5dc585c5
Andy Dm <demarieandy@outlook.com>
2018-01-10 23:14:26 +0100

    Add files via upload

---
+++ alex75/welcomeback/language/nl/welcomebacklang.php
@@ -0,0 +1,23 @@
+<?php
+/** 
+* 
+* @package welcome back
+* @copyright (c) 2015 alex75 www.phpbb-store.it
+* Nederlandse vertaling @ Solidjeuh
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
+* 
+*/ 
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+$lang = array();
+}
+$lang = array_merge($lang, array(
+'WELCOMEBACK_ON'			=> 'Welkom terug op',
+'WELCOMEBACK_HI'			=> 'Hallo',
+'WELCOMEBACK_SEE'			=> 'Leuk je te zien!',
+'WBMY_TOPICS'				=> 'Jouw Topics',
+));
